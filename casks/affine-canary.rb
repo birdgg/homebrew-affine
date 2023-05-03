61f37c9bff60d9915f428acab2e6f8e4e9055f36
birdgg <birdgg@users.noreply.github.com>
2023-05-03 08:47:18 +0000

    Update AFFiNE Canary to ${LATEST_VERSION}

--- casks/affine-canary.rb
+++ casks/affine-canary.rb
@@ -1,10 +1,10 @@
 cask "affine-canary" do
   version "0.5.4-canary.20"
-  sha256 :no_check
-
   if Hardware::CPU.arm?
+    sha256 "ad07480372d5c1b849766a3e377ca94386f64dbcd5185ce531d91056f8e60240"
     url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-canary-macos-arm64.dmg"
   else
+    sha256 "86394dbf95a587ae914b03223abd2c237dd5da16f6958896fd0acb8cfc04283e"
     url "https://github.com/toeverything/AFFiNE/releases/download/v#{version}/affine-canary-macos-x64.dmg"
   end
 
@@ -15,5 +15,4 @@ cask "affine-canary" do
   depends_on macos: ">= :sierra"
 
   app "AFFiNE-canary.app"
-
 end
